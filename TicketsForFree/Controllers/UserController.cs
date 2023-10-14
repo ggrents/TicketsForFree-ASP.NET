8585290e2cac8b7e24f47a0bbae43f6bfbe90f7e
ggrents <chef_grents@mail.ru>
2023-10-14 13:17:58 +0600

    create all crud methods

--- TicketsForFree/Controllers/UserController.cs
+++ TicketsForFree/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TicketsForFree.Models;
 using TicketsForFree.Services;
+using TicketsForFree.ViewModels;
 
 
 namespace TicketsForFree.Controllers
@@ -40,21 +41,28 @@ namespace TicketsForFree.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateUser([FromBody] User newUser)
+        public async Task<IActionResult> CreateUser([FromBody] CreateUserViewModel newUser)
         {
             try
             { 
-                string hashedPassword = BCrypt.Net.BCrypt.HashPassword(newUser.PasswordHash);
+                string hashedPassword = BCrypt.Net.BCrypt.HashPassword(newUser.Password);
 
 
-                newUser.PasswordHash = hashedPassword;
+                var UserToCreate = new User
+                {
+
+                    FirstName = newUser.FirstName,
+                    LastName = newUser.LastName,
+                    Email = newUser.Email,
+                    PasswordHash = hashedPassword
+                };
+
                 
+                
+                var createdUser = await _userService.CreateUser(UserToCreate);
 
 
-                var createdUser = await _userService.CreateUser(newUser);
-
-
-                return Ok(newUser);
+                return Ok(createdUser);
 
             }
             catch (Exception ex)
@@ -71,8 +79,98 @@ namespace TicketsForFree.Controllers
         public  async Task<ActionResult> DeleteUser(int id)
         {
            await _userService.DeleteUser(id);
+            var users = await _userService.GetAll();
+            return Ok(users);
+        }
 
-            return Ok();
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateUser(int id, [FromBody] UpdateUserViewModel updatedUser)
+        {
+            try
+            {
+                if (updatedUser == null)
+                {
+                    return BadRequest(new { message = "User data is invalid" });
+                }
+
+      
+                var existingUser = await _userService.Get(id);
+
+                if (existingUser == null)
+                {
+                    return NotFound(new { message = "User not found" });
+                }
+
+
+                existingUser.FirstName = updatedUser.FirstName;
+                existingUser.LastName = updatedUser.LastName;
+                existingUser.Email = updatedUser.Email;
+
+
+
+
+                var UserToUpdate = await _userService.UpdateUser(id, existingUser);
+
+                return Ok(UserToUpdate);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Failed to update user", error = ex.Message });
+            }
+
+
+            
+        }
+
+
+        [HttpPatch("{id}")]
+
+        public async Task<ActionResult> PartUpdateUser(int id, [FromBody] UpdateUserViewModel updatedUser)
+        {
+            try
+            {
+                if (updatedUser == null)
+                {
+                    return BadRequest(new { message = "User data is invalid" });
+                }
+
+
+                var existingUser = await _userService.Get(id);
+
+                if (existingUser == null)
+                {
+                    return NotFound(new { message = "User not found" });
+                }
+
+
+                if (updatedUser.FirstName != null)
+                {
+                    existingUser.FirstName = updatedUser.FirstName;
+                }
+
+                if (updatedUser.LastName != null)
+                {
+                    existingUser.LastName = updatedUser.LastName;
+                }
+
+                if (updatedUser.Email != null)
+                {
+                    existingUser.Email = updatedUser.Email;
+                }
+
+
+
+                var UserToUpdate = await _userService.UpdateUser(id, existingUser);
+
+                var user = await _userService.Get(id);
+
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Failed to update user", error = ex.Message });
+            }
         }
     }
 }
---
+++ TicketsForFree/ViewModels/UpdateUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketsForFree.ViewModels
+{
+    public class UpdateUserViewModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+ 
+    }
+}
