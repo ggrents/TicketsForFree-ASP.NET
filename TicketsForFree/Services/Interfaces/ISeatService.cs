55cac94b4ce8ab73f068647f1a52117e3f577286
Artem Grents <143025643+ggrents@users.noreply.github.com>
2023-10-15 15:03:51 +0600

    Update ISeatService.cs

--- TicketsForFree/Services/Interfaces/ISeatService.cs
+++ TicketsForFree/Services/Interfaces/ISeatService.cs
@@ -4,24 +4,14 @@ namespace TicketsForFree.Services.Interfaces
 {
     public interface ISeatService
     {
-<<<<<<< HEAD
+
    
         Task<IEnumerable<Seat>> GetAll();
         Task<IEnumerable<Seat>> GetVipSeats();
         Task<IEnumerable<Seat>> GetReservedSeats();
         Task<IEnumerable<Seat>> GetUnreservedSeats();
         Task<Seat> GetById(int id);
-        Task<Seat> AddSeat(Seat seat);
-=======
-        Task<IEnumerable<Seat>> GetSeatsByJourney(int journeyId);
-        Task<IEnumerable<Seat>> GetAll();
-        Task<IEnumerable<Seat>> GetVipSeats(int journeyId);
-        Task<IEnumerable<Seat>> GetReservedSeats(int journeyId);
-        Task<IEnumerable<Seat>> GetUnreservedSeats(int journeyId);
-        Task<Seat> GetById(int id);
-        Task<Seat> AddSeat(Seat seat);
-        Task<Seat> UpdateSeat(Seat seat);
->>>>>>> cb32489a51433bf8958d45d859864c9408c31f75
+        Task<Seat> AddSeat(Seat seat)
         Task<bool> DeleteSeat(int id);
     }
 }
